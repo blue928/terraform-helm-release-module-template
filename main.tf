c5b37ddb0fcf45d594cd96059b907c17e85cea74
Blue Presley <bluepresley@gastonia.com>
2022-04-21 11:11:14 -0400

    update

--- main.tf
+++ main.tf
@@ -1,1 +1,32 @@
+resource "helm_release" "ingress_nginx" {
+  name       = "ingress-nginx"
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  version = var.chart_version
 
+  # todo change this to a variable
+  namespace        = "ingress-nginx"
+  create_namespace = true
+  timeout          = 1500
+  #atomic  = true
+  #cleanup_on_fail = true
+
+  #values = [file("nginx_ingress_values.yaml")]
+
+  set {
+    name  = "controller.replicaCount"
+    value = "2"
+  }
+
+  set {
+    name  = "controller.service.externalTrafficPolicy"
+    value = "Local"
+  }
+
+  #set {
+    #name = "controller.service.loadBalancerIP"
+    #value = azurerm_public_ip.lb_public_ip.ip_address
+    #value = module.aks-cluster.lb_public_ip
+    #value = "var.lb_public_ip"
+  #}
+}
--- variables.tf
+++ variables.tf
@@ -1,1 +1,4 @@
-
+variable "chart_version" {
+  type    = string
+  default = "4.0.19"
+}
